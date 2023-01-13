c6420de4b145d4a36b8e2f2d1aa4fcb905dc8dbf
Kedllon Kleber <50781783+kkleber1991@users.noreply.github.com>
2023-01-13 20:11:00 -0300

    creation app
    Criando o projeto para dar inicio na view

---
+++ lib/app/view/format_text.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class FormatText extends StatefulWidget {
+  const FormatText({super.key});
+
+  @override
+  State<FormatText> createState() => _FormatTextState();
+}
+
+class _FormatTextState extends State<FormatText> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
